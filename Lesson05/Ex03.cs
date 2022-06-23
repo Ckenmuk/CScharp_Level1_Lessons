fad302505e5853b1216c64435afaa26c0c54a8a6
Dolgov Mikhail <dr.dolohoff@yandex.ru>
2022-06-23 21:10:39 +0700

    Seminar Part01

---
+++ Lesson05/Ex03.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ckenmuk.Test.Library;
+
+namespace Lesson05
+{
+    internal class Ex03
+    {
+        static void Main(string[] args)
+        {
+            OutputHelpers.PrintDemo(5, 3);
+
+            Console.WriteLine(@"Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+Например:
+badc являются перестановкой abcd.
+");
+            string text1;
+
+            string text2;
+
+            Console.WriteLine("Введите строку №1");
+            text1 = Console.ReadLine();
+
+            Console.WriteLine("Введите строку №2");
+            text2 = Console.ReadLine();
+
+            int counter = 0;
+            
+            for (int i = 0; i < text1.Length; i++)
+            {
+                for (int j = 0; j < text2.Length; j++)
+                {
+                    if ((text1[i].Equals(text2[j])))
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            if (counter == text1.Length)  Console.WriteLine($"Строка {text1} является перестановкой строки {text2}"); 
+            else Console.WriteLine($"Строка {text1} не является перестановкой строки {text2}");
+
+            // не работает, если символы в строке повторяются, например абвга и агваб. Не понимаю, как этого избежать...
+        }
+    }
+}
---
+++ Lesson06/Sample01.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06
+{
+    internal class Sample01
+    {
+
+        static void PrintDir(DirectoryInfo dir, string indent, bool lastDirectory)
+        {
+            Console.Write(indent);
+            if (lastDirectory)
+            {
+                Console.Write("└─");
+                indent += "  ";
+            }
+            else
+            {
+                Console.Write("├─");
+                indent += "│  ";
+            }
+
+            Console.WriteLine(dir.Name);
+
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+
+            FileInfo[] subFiles = dir.GetFiles();
+
+            // вывести иерархию вместе с файлами
+
+            for (int i = 0; i < subDirs.Length; i++)
+            {
+                PrintDir(subDirs[i], indent, i == subDirs.Length - 1);
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Directory, File, FileInfo, DirectoryInfo
+
+            if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory))
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+                Console.WriteLine($"FullName: {directoryInfo.FullName}");
+                Console.WriteLine($"Name: {directoryInfo.Name}");
+                Console.WriteLine($"Parent: {directoryInfo.Parent}");
+                Console.WriteLine($"CreationTime: {directoryInfo.CreationTime}");
+                Console.WriteLine($"Root: {directoryInfo.Root}");
+
+                
+            }
+
+
+            PrintDir(new DirectoryInfo(@"D:\My_projects\!GeekBrains\4 Основы С#\CScharp_Level1_Lessons\Lesson06"), "", true);
+            
+        
+        }
+    }
+}
