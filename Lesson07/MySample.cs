15ccb3a24fe78edfef58f02e2784cfba356501dd
Dolgov Mikhail <dr.dolohoff@yandex.ru>
2022-06-27 21:54:34 +0700

    Actualization

---
+++ Lesson07/MySample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson07
+{
+    partial class MySample
+    {
+        public int B { get; set; }
+    }
+}
---
+++ Lesson07/Sample01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson07
+{
+
+    partial class MySample
+    {
+        public int A { get; set; }
+    }
+
+
+    internal class Sample01
+    {
+
+        static void Main(string[] args)
+        {
+            MySample sample = new MySample();
+            sample.A = 1;
+            sample.B = 2;
+
+        }
+    }
+}
